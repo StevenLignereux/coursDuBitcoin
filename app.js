2e2735eb1568243fe5004ef55f22ab97b56c9cfe
StevenLignereux <steven.lignereux@hotmail.fr>
2020-08-10 18:29:34 +0200

    optimisation et actualisation

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const url = 'https://blockchain.info/ticker';
 
-let requete =  new XMLHttpRequest();
 
-requete.open('GET',url);
-requete.responseType = 'json';
-requete.send();
+function reccupererPrix(){
+    let requete =  new XMLHttpRequest();
+
+    requete.open('GET',url);
+    requete.responseType = 'json';
+    requete.send();
 
 
-requete.onload = function () { 
+    requete.onload = function () { 
 
-    if (requete.readyState === XMLHttpRequest.DONE) {
-        
-        if (requete.status === 200) {
+        if (requete.readyState === XMLHttpRequest.DONE) {
             
-            let reponse = requete.response;
-            let prixEnEuros = reponse.EUR.last;
-            
-            document.querySelector('#price_label').textContent = prixEnEuros;
+            if (requete.status === 200) {
+                
+                let reponse = requete.response;
+                let prixEnEuros = reponse.EUR.last;
+                
+                document.querySelector('#price_label').textContent = prixEnEuros;
 
-        } else {
+            } else {
 
-            alert('Un problème est survenu, merci de revenir plus tard');
+                alert('Un problème est survenu, merci de revenir plus tard');
 
+            }
         }
     }
-};
+
+    console.log('Prix actualisé');
+}
+
+setInterval(reccupererPrix, 1000);
